37524d25d3eefc4ad6d5f6284ccbb1f8a8deecc2
miloske <nikola.milosevic060@gmail.com>
2026-01-19 19:28:36 +0100

    starter page layout

---
+++ client/src/app/(site)/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+      <main className="h-screen w-screen bg-black grid grid-cols-1 p-4 pb-12 md:p-8 lg:p-16">
+          <button className="bg-white box-shadow absolute top-4 right-4 w-12 h-12 md:w-16 md:h-16 rounded-full"></button>
+          <button className="bg-green-400 h-12 w-64 md:h-16 md:w-96 lg:h-24 lg:w-128 self-end justify-self-center rounded-sm"></button>
+      </main>
+    );
+  }
+  
